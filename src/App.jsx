62af9a4312d7a83eace6e0efc67089507d18e222
mdasif8393 <md.asif.8393@gmail.com>
2024-04-19 10:38:35 +0600

    add all dumy ui component from tamplate to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,18 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1.25rem"
+      },
+      colors: {
+        body: "#1D1E28",
+        light: "#fff",
+      }
+    },
   },
   plugins: [],
 }
